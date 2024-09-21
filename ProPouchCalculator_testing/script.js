b0f361fa6711b6225cc226655cb2690e1d821f89
ManorInnDND <95050755+ManorInnDND@users.noreply.github.com>
2024-09-21 12:00:08 -0400

    lot scanner update. first pas cannot test cause cors. might work at work

--- ProPouchCalculator_testing/script.js
+++ ProPouchCalculator_testing/script.js
@@ -3,12 +3,10 @@
 // TODO set up with lot number loading
 
 const LOT_TABLE_WEB_APP_BASE_URL = "https://script.google.com/macros/u/1/s/AKfycbxItr56E2Q-pgWhYB8yBig1REeoEPjXN3EPSDV6iSn6V09eCX-Q7CXgC5Qp0W3oeykI/exec"
-const LOT_TABLE_WEB_APP_LOT_GET_PREFIX = "?lot="
-const LOT_DATA_WEB_APP_GET_COMMAND = "?command=getSavedSearchDataById&Id=10090"
-const LOT_DATA_WEB_APP_GET_HARVEST_DATA_COMMAND = LOT_TABLE_WEB_APP_BASE_URL + LOT_DATA_WEB_APP_GET_COMMAND
+const LOT_DATA_WEB_APP_GET_LITERS_PRODUCED_DATA_URL =LOT_TABLE_WEB_APP_BASE_URL + "?command=getSavedSearchDataById&Id=9756"
+const LOT_DATA_WEB_APP_GET_LITERS_WORK_ORDER_DATA_URL = LOT_TABLE_WEB_APP_BASE_URL + "?command=getSavedSearchDataById&Id=9738"
 
-
-var testData = {"columns":["Work Order","Strain","Harvest Date (Mfg Date)","Cell Number","Lot","Liters Produced","Yeast Pack Height (cm)","Trial or Deviation Notes"],"data":[["Work Order #267942","WLP001","09/20/2024","2602","22679420","23.2","4",""],["Work Order #267942","WLP001","09/20/2024","2604","22679421","26.1","4",""],["Work Order #267943","WLP007","09/20/2024","2606","22679430","29.2","6",""],["Work Order #267944","WLP008","09/20/2024","2608","22679440","23.7","10.5",""],["Work Order #267945","WLP029","09/20/2024","2610","22679450","29.5","4.6",""],["Work Order #267945","WLP029","09/20/2024","2612","22679451","27.5","5.3",""],["Work Order #267946","WLP066","09/20/2024","2614","22679460","17.9","6.2",""],["Work Order #267946","WLP066","09/20/2024","2616","22679461","19.5","8",""],["Work Order #267947","WLP077","09/20/2024","2002","22679470","22","5.9",""],["Work Order #267948","WLP090","09/20/2024","2004","22679480","18.9","4",""],["Work Order #267948","WLP090","09/20/2024","2006","22679481","18.8","3.5",""],["Work Order #267949","WLP300","09/20/2024","2008","22679490","18.7","5.5",""],["Work Order #267950","WLP400","09/20/2024","2010","22679500","9.4","2.4",""],["Work Order #267950","WLP400","09/20/2024","2012","22679501","9.5","3",""],["Work Order #267951","WLP500","09/20/2024","2014","22679510","19.5","2.3",""],["Work Order #267952","WLP740","09/20/2024","2016","22679520","22.6","5.3",""],["Work Order #267953","WLP800","09/20/2024","2018","22679530","20","4.7",""],["Work Order #267954","WLP830","09/20/2024","2020","22679540","16.5","3.5",""],["Work Order #267955","WLP840","09/20/2024","2022","22679550","26.5","4.2",""]]}
+var litersProducedData = {"columns":["Work Order","Strain","Harvest Date (Mfg Date)","Cell Number","Lot","Liters Produced","Yeast Pack Height (cm)","Trial or Deviation Notes"],"data":[["Work Order #267942","WLP001","09/20/2024","2602","22679420","23.2","4",""],["Work Order #267942","WLP001","09/20/2024","2604","22679421","26.1","4",""],["Work Order #267943","WLP007","09/20/2024","2606","22679430","29.2","6",""],["Work Order #267944","WLP008","09/20/2024","2608","22679440","23.7","10.5",""],["Work Order #267945","WLP029","09/20/2024","2610","22679450","29.5","4.6",""],["Work Order #267945","WLP029","09/20/2024","2612","22679451","27.5","5.3",""],["Work Order #267946","WLP066","09/20/2024","2614","22679460","17.9","6.2",""],["Work Order #267946","WLP066","09/20/2024","2616","22679461","19.5","8",""],["Work Order #267947","WLP077","09/20/2024","2002","22679470","22","5.9",""],["Work Order #267948","WLP090","09/20/2024","2004","22679480","18.9","4",""],["Work Order #267948","WLP090","09/20/2024","2006","22679481","18.8","3.5",""],["Work Order #267949","WLP300","09/20/2024","2008","22679490","18.7","5.5",""],["Work Order #267950","WLP400","09/20/2024","2010","22679500","9.4","2.4",""],["Work Order #267950","WLP400","09/20/2024","2012","22679501","9.5","3",""],["Work Order #267951","WLP500","09/20/2024","2014","22679510","19.5","2.3",""],["Work Order #267952","WLP740","09/20/2024","2016","22679520","22.6","5.3",""],["Work Order #267953","WLP800","09/20/2024","2018","22679530","20","4.7",""],["Work Order #267954","WLP830","09/20/2024","2020","22679540","16.5","3.5",""],["Work Order #267955","WLP840","09/20/2024","2022","22679550","26.5","4.2",""]]}
 
 var litersOrLotInput = "";
 var numberHBInput = "";
@@ -84,60 +82,94 @@ class HarvestDataHandler{
      */
     refreshAndSetLot(lotNumber){
         this.lotNumber = lotNumber;
-        this.getSavedSearchData();
+        this.getLitersProducedData();
     };
 
-    async getSavedSearchData(){
+    async getLitersProducedData(){
         //TODO test me
         var req = new XMLHttpRequest();
-        var reqURL = LOT_DATA_WEB_APP_GET_HARVEST_DATA_COMMAND
+        var reqURL = LOT_DATA_WEB_APP_GET_LITERS_PRODUCED_DATA_URL
         req.open("GET", reqURL);
         req.send();
-        await sleep(3000) // this may need to increase to allow load to occur
-        req.onload = this.onLotTableLoad(event);
+        //await sleep(3000) // this may need to increase to allow load to occur 
+        req.onload = this.onLitersProducedLoad(event);
     }
 
-    onLotTableLoad(event){
+    onLitersProducedLoad(event){
         console.log(event)
         let xhr = event.currentTarget
         if (xhr.readyState === xhr.DONE) {
             if (xhr.status === 200) {
                 var lotTableArray = JSON.parse(xhr.responseText);
-                harvestDataHandler.updateProPouchInputValues(lotTableArray);
+                harvestDataHandler.getLitersProducedFromTable(lotTableArray);
+            };
+          };
+    };
+
+    /**
+    * Build the lot table
+    * @param {{columns: string[]; data: string[][]}} lotTableArray 
+    */
+    getLitersProducedFromTable(lotTableArray){
+        var columnHeaders = lotTableArray.columns;
+        var data = lotTableArray.data;
+        var lotIndex = columnHeaders.indexOf("lot"); 
+        var litersProducedIndex = columnHeaders.indexOf("liters_produced");
+        var lotRow = data.filter((x)=> x[lotIndex] == this.lotNumber);
+        var litersProduced = parseFloat(lotRow[litersProducedIndex]);
+        this.litersProduced = litersProduced;
+        this.getWorkOrdersData();
+        
+    };
+    
+    async getWorkOrdersData(){
+        //TODO test me
+        var req = new XMLHttpRequest();
+        var reqURL = LOT_DATA_WEB_APP_GET_LITERS_WORK_ORDER_DATA_URL
+        req.open("GET", reqURL);
+        req.send();
+        //await sleep(3000) // this may need to increase to allow load to occur 
+        req.onload = this.onWorkOrdersLoad(event);
+    }
+
+    onWorkOrdersLoad(event){
+        console.log(event)
+        let xhr = event.currentTarget
+        if (xhr.readyState === xhr.DONE) {
+            if (xhr.status === 200) {
+                var lotTableArray = JSON.parse(xhr.responseText);
+                this.getPackSizesFromTableData(lotTableArray);
             };
           };
     };
     /**
     * Build the lot table
-    * @param {Array} lotTableArray 
+    * @param {{columns: string[]; data: string[][]}} lotTableArray 
     */
-    updateProPouchInputValues(lotTableArray){
+    getPackSizesFromTableData(lotTableArray){
         var columnHeaders = lotTableArray.columns;
-        var quantityIndex = columnHeaders.indexOf("quantity"); 
-        var sizeIndex = columnHeaders.indexOf("size");
-        var litersProducedIndex = columnHeaders.indexOf("liters_produced")
-        var litersProduced = parseFloat(lotTableArray[0][litersProducedIndex])
-        this.calculateProPouches(lotTableArray, sizeIndex, quantityIndex, litersProduced)
+        var data = lotTableArray.data;
+        var lotIndex = columnHeaders.indexOf("lot"); 
+        var quantityIndex = columnHeaders.indexOf("quantity");
+        var itemNameIndex = columnHeaders.indexOf("item_name");
+        var lotRows = data.filter((x)=> x[lotIndex] == this.lotNumber);
+        this.numHB = 0;
+        this.numNano = 0;
+        for (var lotRow of lotRows){
+            var itemName = lotRow[itemNameIndex];
+            if (itemName.endsWith("Nano")){this.numNano = parseInt(lotRow[quantityIndex])}
+            else if (itemName.endsWith("HB")){this.numHB = parseInt(lotRow[quantityIndex])}
+        };
+        this.calculateProPouches()
+        
     };
-    /** 
-     * @param {Array[]} lotTableArray
-     * @param {Number} litersProduced 
-     * @param {Number} quantityIndex 
-     * @param {Number} sizeIndex 
-     * @return {Number}
-      */
-    calculateProPouches(lotTableArray, sizeIndex, quantityIndex, litersProduced){
-        let hbData = lotTableArray.find((x) => x[sizeIndex] == "Homebrew");
-        let nanoData = lotTableArray.find((x) => x[sizeIndex] == "Nano");
-        var numNano = 0;
-        var numHB = 0;
-        if (hbData) {numHB = hbData[quantityIndex]};
-        if (nanoData) {numNano = nanoData[quantityIndex]};
-        let numPro = Math.floor((litersProduced - (numHB * 0.07) - (numNano * 0.35))/1.75)
+
+    calculateProPouches(){
+        let numPro = Math.floor((this.litersProduced - (this.numHB * 0.07) - (this.numNano * 0.35))/1.75)
         numberHBInput.value = numHB
         numberNanoInput.value = numNano
         numberOfProOutput.value = numPro
-    }
+    };
 };
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
