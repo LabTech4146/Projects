ca51aef361ffcce849d3707b92641ada7979a973
LabTech4146 <159316124+LabTech4146@users.noreply.github.com>
2025-05-13 15:35:28 -0400

    Update monkeyModel.js

--- tamperMonkey/monkeyModel.js
+++ tamperMonkey/monkeyModel.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RFSmart Custom Field Dump Magic
 // @namespace    http://tampermonkey.net/
-// @version      2025-05-13
+// @version      2025-05-13b
 // @description  Provide label printing enhancements.
 // @author       You
 // @match        https://4099054.app.netsuite.com/app/site/hosting/scriptlet.nl?script=customscript_rfs_controller&deploy=customdeploy_rfs_controller&file=1459763*
